8b8c156c67e938f0ecadffff2601027b127c1e8e
Aleksandr Pakhmutov <a.pakhmutov@criteo.com>
2022-01-17 18:29:29 +0100

    More renderings

--- src/App.js
+++ src/App.js
@@ -4,33 +4,67 @@ import data from './data.json';
 function App() {
   console.log(data);
   return (
-    <div>
-      {renderClans(data)}
+    <div className="root">
+      <div className="header">Header</div>
+      <div className="content">
+        {renderAllies(data)}
+      </div>
+      <div className="footer">
+        Footer
+      </div>
     </div>
   );
 }
 
-function renderClans(data) {
-  if (data.clans != null && data.clans.length > 0) {
+function renderAllies() {
+  if (data.allies != null && data.allies.length > 0) {
     return (
-      <div id="clans">
-        {data.clans.map(c => renderClan(c))}
+      <div>
+        {data.allies.map((ally, idx) => renderAlly(ally, idx))}
       </div>
     );
   }
 }
 
-function renderClan(clan) {
+function renderAlly(ally, idx) {
+  if (ally.clans != null && ally.clans.length > 0) {
+    return (
+      <table className="ally" key={idx}>
+        <thead className="allyTableHeader">
+          <tr>
+            <th className="clanNameHeader">{ally.name}</th>
+            <th className="clHeader">LEADER</th>
+            <th className="otherColumn">CASTLE</th>
+            <th className="otherColumn">CLAN HALL</th>
+            <th className="otherColumn">PRIORITY</th>
+            <th className="otherColumn">MEMBERS</th>
+          </tr>
+        </thead>
+        <tbody>
+          {ally.clans.map((x, idx) => renderClan(x, idx))}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+function renderClan(clan, idx) {
   return (
-    <div>
-      <div>{clan.name}</div>
-      <div>{clan.clName}</div>
-      <div>{clan.castle}</div>
-      <div>{clan.clanHall}</div>
-      <div>{clan.goal}</div>
-      <div>{clan.population}</div>
-    </div>
+    <tr key={idx}>
+      <th className="clanName">{clan.name}</th>
+      <th className="cl">{clan.clName}</th>
+      <th className={clan.castle === "NONE" ? "empty" : "castle"}>{clan.castle}</th>
+      <th className={clan.clanHall === "NONE" ? "empty" : "ch"}>{clan.clanHall}</th>
+      <th className={getGoalClassName(clan.goal)}>{clan.goal}</th>
+      <th>{clan.population}</th>
+    </tr>
   );
+}
+
+function getGoalClassName(value) {
+  if (value === "PVP") return "goalPurple";
+  if (value === "PVE") return "goalWhite";
+  return "empty";
 }
 
 export default App;
